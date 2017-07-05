be1c971ac7faae1f36085fe8523268670280da2c
Kevin-Geoffery O'Hearon <kohearon@modernmedia.ca>
2017-07-05 09:24:02 -0300

    Basic browserify and minification task.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,19 @@
 var gulp = require('gulp');
+
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
-
 var presets = [
   autoprefixer({browsers: ['last 3 versions']}),
   cssnano()
 ];
+
+var source = require('vinyl-source-stream');
+var browserify = require('browserify')
+var buffer = require("vinyl-buffer");
+var gutil = require("gulp-util");
+var uglify = require('gulp-uglify');
 
 gulp.task('styles', function(){
   return gulp.src('index.scss')
@@ -16,8 +22,22 @@ gulp.task('styles', function(){
     .pipe(gulp.dest('./'))
 });
 
-gulp.task('watch', function(){
-  gulp.watch('*.scss', ['styles']);
+gulp.task('scripts', function(){
+  var b = browserify({
+    entries: 'index.js',
+  });
+
+  return b.bundle()
+   .pipe(source('index.js'))
+   .pipe(buffer())
+   .pipe(uglify())
+   .on("error", gutil.log)
+   .pipe(gulp.dest('dist/'))
 });
 
-gulp.task('default', ['styles', 'watch']);
+gulp.task('watch', function(){
+  gulp.watch('*.scss', ['styles']);
+  gulp.watch('*.js', ['scripts']);
+});
+
+gulp.task('default', ['styles', 'scripts', 'watch']);
